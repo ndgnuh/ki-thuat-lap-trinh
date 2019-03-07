3ae7abc5d5c49453c14efc30c3184d5afaa00648
hung <ndgnuh99@gmail.com>
2019-03-07 19:24:38 +0700

    Add quick sort

--- tuan5/QuickSort.cs
+++ tuan5/QuickSort.cs
@@ -1,15 +1,49 @@
 using System; 
 using System.Collections.Generic;
+using System.Linq;
 
 class QSDNC 
 {
-
   public static void Main () {
-    List<int> MyList = new List<int> {4,3,5,9,7,4,1,3,4,5,2,3}
-
+    List<int> ListToSort = new List<int> {3,4,1,5,1,35,3,12,2412,3};
+    List<int> SortedList = Sort(ListToSort.ToList(), 0, ListToSort.Count-1);
+    Console.WriteLine("List before sorted:");
+    PrettyPrint(ListToSort);
+    Console.WriteLine("Sorted List");
+    PrettyPrint(SortedList);
   }
 
-  private static List<int> QuickSort(List<int>) {
+  private static  List<int> Sort(List<int> MyList, int FirstIndex, int LastIndex) {
+    if(FirstIndex < LastIndex) {
+      int PartIndex = Part(MyList, FirstIndex, LastIndex);
+      Sort(MyList, FirstIndex, PartIndex-1);
+      Sort(MyList, PartIndex+1, LastIndex);
+    }
+    return MyList;
+  }
 
+  private static int Part(List<int> MyList, int FirstIndex, int LastIndex) {
+    int Pivot = MyList[LastIndex];
+    int i = FirstIndex - 1;
+    for(int j = FirstIndex; j < LastIndex; j++) {
+      if(MyList[j] <= Pivot) 
+        { i ++;  Swap(MyList, i, j); }
+    }
+    Swap(MyList, i+1, LastIndex);
+    return i + 1;
+  }
+
+  private static void Swap(List<int> MyList, int Index1, int Index2) {
+    int Swap = MyList[Index1];
+    MyList[Index1] = MyList[Index2];
+    MyList[Index2] = Swap;
+  }
+
+  private static void PrettyPrint(List<int> MyList) {
+    Console.Write("{");
+    for(int i = 0; i < MyList.Count - 1; i++) {
+      Console.Write(MyList[i] + ", ");
+    }
+    Console.WriteLine(MyList[MyList.Count-1] + "}");
   }
 }
