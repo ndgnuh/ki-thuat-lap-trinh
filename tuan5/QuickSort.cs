1853d441c6f4de7441a4022e0b420c73aa57e8e0
hung <ndgnuh99@gmail.com>
2019-03-07 18:45:55 +0700

    ...

---
+++ tuan5/QuickSort.cs
@@ -0,0 +1,15 @@
+using System; 
+using System.Collections.Generic;
+
+class QSDNC 
+{
+
+  public static void Main () {
+    List<int> MyList = new List<int> {4,3,5,9,7,4,1,3,4,5,2,3}
+
+  }
+
+  private static List<int> QuickSort(List<int>) {
+
+  }
+}
--- tuan5/Reference.cs
+++ tuan5/Reference.cs
@@ -1,14 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 class DemoReference 
 {
   public static void Main () {
     int SomeRandomVar = 0;
+    List<int> RandomList = new List<int> {1,2,3};
+
     Console.WriteLine("Old value: " + SomeRandomVar);
     PassByVal(SomeRandomVar);
     Console.WriteLine("Value after passing by value: " + SomeRandomVar);
     PassByRef(ref SomeRandomVar);
     Console.WriteLine("Value after passing by reference: " + SomeRandomVar);
+
+    Console.WriteLine("\nList are passed as reference: \n\tOld List:");
+    RandomList.ForEach(Console.WriteLine);
+    Console.WriteLine("\tNew List:");
+    PassList(RandomList);
+    RandomList.ForEach(Console.WriteLine);
+
+
   }
 
   private static void PassByRef(ref int MyRef) {
@@ -17,5 +28,9 @@ class DemoReference
 
   private static void PassByVal(int MyVal) {
     MyVal += 10;
+  }
+
+  private static void PassList(List<int> MyList) {
+    MyList.Add(4);
   }
 }
