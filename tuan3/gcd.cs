133828373bc79b6d9d47fbd342b26086332fbd6d
hung <ndgnuh99@gmail.com>
2019-03-23 20:44:27 +0700

    remaster

--- tuan3/gcd.cs
+++ tuan3/gcd.cs
@@ -1,50 +1,44 @@
 using System;
 using System.Collections.Generic;
 
-public static class Program {
-    public static int Main () {
-        int a = 16*8; int b = 24332;
-        Console.WriteLine("Gcd(" + a + "," + b + ") = " + Gcd(a,b));
-        return 0;
+class DynamicCombinator 
+{
+  private static List<double> FactorialList = new List<double> {1};
+
+  public static void Main () {
+    char choice;
+    do {
+      Console.Clear();
+
+      Console.Write("[input] n: "); 
+      int n = Convert.ToInt32(Console.ReadLine());
+
+      Console.Write("[input] k: "); 
+      int k = Convert.ToInt32(Console.ReadLine());
+
+      if (n > 170) Console.WriteLine("[warning] " + n + "! is very large, C# may not handle it");
+
+      Console.WriteLine("[output] " + n + "C" + k + ": " + Combinator(k,n));
+      Console.WriteLine("Continue (y/n)?");
+      choice = Convert.ToChar(Console.Read());
+    }
+    while(choice == 'y');
+    Console.Clear();
+  }
+
+  private static double Combinator (int k, int n)  {
+    if(k > n) {
+      Console.WriteLine("Invalid input!");
+      return 0;
     }
 
-    private static int Gcd(int a, int b) {
-        return GcdLoop(a, b);
+    for(int i = FactorialList.Count; i <= n; i++) {
+      FactorialList.Add(FactorialList[i-1]*i);
     }
 
-    private static int GcdRcs(int a, int b) {
-        return b % a == 0 ? a : GcdRcs(b % a, a);
-    }
+    return FactorialList[n]
+      / FactorialList[k]
+      / FactorialList[n-k];
+  }
 
-    private static int GcdLoop(int a, int b) {
-        while (true){
-            if ( b % a == 0 ) return a;
-            if ( a % b == 0 ) return b;
-            if ( b > a )
-                b = b % a;
-            else
-                a = a % b;
-        }
-    }
-
-    private static int GcdDyn(int a, int b) {
-        var u = new List<int> {a, b};
-        int len = u.Count;
-        int c, d;
-        while(true) {
-            if ( u[len - 2] % u[len - 1] == 0) {
-                return u[len-1];
-            }
-            if ( u[len-1] % u[len-2] == 0) {
-                return u[len-2];
-            }
-            len = u.Count;
-            c = u[len-2];
-            d = u[len-1];
-            if ( d > c )
-                u.Add(d % c);
-            else
-                u.Add(c % d);
-        }
-    }
 }
--- tuan7/Sqrt2.cs
+++ tuan7/Sqrt2.cs
@@ -3,23 +3,29 @@ using System;
 class SqrtOfTwo 
 {
   public static void Main () {
-    Console.WriteLine("sqrt(2) = " + Sqrt2(10e-4));
+    double x = 625;
+    const double tol = 10e-6;
+    Console.WriteLine("sqrt(2) = " + Sqrt2(tol));
+    Console.WriteLine("sqrt(" + x + ") = " + Sqrt(x, tol));
   }
 
   private static double Sqrt2(double tol) {
-    // solve for x^2 - 2 = 0
+    // solve for f(x) = x^2 - 2 = 0
     double x = 1;
-    while(Math.Abs(f(x)) > tol) {
-      x = x - f(x)/df(x);
+    while(Math.Abs(x*x - 2) > tol) {
+      // x - f(x) / f'(x) = x - (x^2 - 2) / (2x) = x/2 + 1/x
+      x = x/2 + 1/x;
     }
     return x;
   }
 
-  private static double f(double x) {
-    return x*x - 2;
-  }
-
-  private static double df(double x) {
-    return 2*x;
+  private static double Sqrt(double y, double tol) {
+    // solve for f(x) = x^2 - 2 = 0
+    double x = y/2;
+    while(Math.Abs(x*x - y) > tol) {
+      // x - f(x) / f'(x) = x - (x^2 - 2) / (2x) = x/2 + 1/x
+      x = x/2 + y/x/2;
+    }
+    return x;
   }
 }
