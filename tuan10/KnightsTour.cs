ef376b82a2a8ee1c6ba9251e49978f62583c18e0
hung <ndgnuh99@gmail.com>
2019-04-13 14:11:32 +0700

    Non square board

--- tuan10/KnightsTour.cs
+++ tuan10/KnightsTour.cs
@@ -5,9 +5,11 @@ namespace KnightsTour
 {
  class Program 
   {
-    static public int width = 4;
-    public static int n = width + 4;
-    public static int iMax = n*n;
+    static public int width = 5;
+    public static int height = 3;
+    public static int n = height + 4; /* 2 border sentinel values */
+    public static int m = width + 4;
+    public static int iMax = n*m;
     public static int startPos = 3*n + 4;
     public static int Count = 0;
     public static int SolutionCount = 0;
@@ -17,6 +19,7 @@ namespace KnightsTour
     public static void Main(){
       Prepare();
       Try(startPos);
+      Console.WriteLine("Total numer of solutions: " + SolutionCount);
     }
 
     public static void Prepare(){
@@ -25,14 +28,14 @@ namespace KnightsTour
         Path.Add(-1);
       }
       for(int i = 2; i < n-2; i++) {
-        for(int j = 2; j < n-2; j++)
+        for(int j = 2; j < m-2; j++)
+          /* 2x2 border sentinel values, inside are free */
           Free[i*n+j] = true;
       }
       Free[startPos] =false;
     }
 
     public static void Try(int i) {
-        // PrintBoard();
         List<int> moves = new List<int> {
           i - n - 2, 
           i - n + 2,
@@ -44,7 +47,7 @@ namespace KnightsTour
           i + 2*n + 1
         };
         foreach(int j in moves) {
-          if(Free[j]) {
+          if(Free[j]) { 
             Count += 1;
             Free[j] = false;
             Path[j] = Count;
@@ -53,32 +56,26 @@ namespace KnightsTour
             Path[j] = -1;
             Count -= 1;
           }
-          else if(Count > 2 && j == startPos )
-          {
+          if(Count > 2 && j == startPos ){
+            SolutionCount+=1;
             PrintBoard();
           }
         }
-
     }
-    // public static void PrintBoard() {
-    //   for(int i = 0; i < iMax; i ++){
-    //     if(Free[i]) Console.Write("|1");
-    //     else Console.Write("|0");
-    //     if((i + 1) % n == 0) Console.WriteLine("|");
-    //   }
-    // }
 
     public static void PrintBoard(bool all = false){
-      for(int i = 0; i < n; i++) Console.Write("###");
+      for(int i = 0; i < m; i++) Console.Write("###");
       Console.WriteLine("");
 
       int start = all ? 0 : 2;
       int end = all ? n : n -2;
+      int cend = all ? m : m -2;
 
       for(int i = start; i < end; i++) {
-        for(int j = start; j < end; j++)
+        for(int j = start; j < cend; j++)
           if (i*n + j == startPos) Console.Write("|  0");
           else if(Path[i*n+j] > 0) Console.Write("|" + Convert.ToString(Path[i*n+j]).PadLeft(3));
+          else if(!Free[i*n+j]) Console.Write("|@@@");
           else Console.Write("|   ");
         Console.WriteLine("|");
       }
