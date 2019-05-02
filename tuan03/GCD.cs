94e2622a7cde1f9e89b5682e21141b7412097460
ndgnuh <ndgnuh99@gmail.com>
2019-05-03 00:57:07 +0700

    Missing file

--- tuan03/GCD.cs
+++ tuan03/GCD.cs
@@ -1,44 +1,23 @@
 using System;
-using System.Collections.Generic;
 
-class DynamicCombinator 
+public class GcdFinder
 {
-  private static List<double> FactorialList = new List<double> {1};
+	public static int Main() 
+	{
+		Console.Write("a = ");
+		int a = Convert.ToInt32(Console.ReadLine());
+		Console.Write("b = ");
+		int b = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("UCLN(a,b) = " + Gcd(a,b));
+		return 0;
+	}
 
-  public static void Main () {
-    char choice;
-    do {
-      Console.Clear();
-
-      Console.Write("[input] n: "); 
-      int n = Convert.ToInt32(Console.ReadLine());
-
-      Console.Write("[input] k: "); 
-      int k = Convert.ToInt32(Console.ReadLine());
-
-      if (n > 170) Console.WriteLine("[warning] " + n + "! is very large, C# may not handle it");
-
-      Console.WriteLine("[output] " + n + "C" + k + ": " + Combinator(k,n));
-      Console.WriteLine("Continue (y/n)?");
-      choice = Convert.ToChar(Console.Read());
-    }
-    while(choice == 'y');
-    Console.Clear();
-  }
-
-  private static double Combinator (int k, int n)  {
-    if(k > n) {
-      Console.WriteLine("Invalid input!");
-      return 0;
-    }
-
-    for(int i = FactorialList.Count; i <= n; i++) {
-      FactorialList.Add(FactorialList[i-1]*i);
-    }
-
-    return FactorialList[n]
-      / FactorialList[k]
-      / FactorialList[n-k];
-  }
-
+	public static int Gcd(int a, int b) 
+	{
+		if (a == 0) return b;
+		if (b == 0) return a;
+		if (a == b) return a;
+		if (a > b) return Gcd(a % b, b);
+		return Gcd(b % a, a);
+	}
 }
