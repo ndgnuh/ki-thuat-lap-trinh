7ccb664f966b08ddff95b5c8dba7acc836d865c0
ndgnuh <ndgnuh99@gmail.com>
2019-05-08 22:33:30 +0700

    Remaster: Newton Method

---
+++ cuoi-ki/NewNewton.cs
@@ -0,0 +1,58 @@
+using System;
+
+public class NewtonSolver
+{
+	public static double tol = 10e-6;
+
+	public static void Main()
+	{
+		Func<double, double> f = (double x) => {
+			return x*x - 2;
+		};
+		Func<double, double> df = (double x) => {
+			return 2*x;
+		};
+
+		SolveV1(f, df, 1, 1);
+		SolveV2(f, df, 1, 4, 1);
+
+	}
+
+	public static double SolveV1(Func<double, double> f, Func<double, double> df, double mindf, double x)
+	{
+		while(Math.Abs(f(x))/mindf >= tol)
+		{
+			if(df(x) == 0)
+			{
+				Console.WriteLine("Error, df/dx == 0 where x = " + x);
+				return -1;
+			}
+			x = x - f(x)/df(x);
+		}
+		Console.WriteLine("x = " + x);
+		return x;
+	}
+
+	public static double SolveV2(Func<double, double> f, Func<double, double> df, double mindf, double maxdf, double x)
+	{
+		if(df(x) == 0)
+		{
+			Console.WriteLine("Error, df/dx == 0 where x = " + x);
+			return -1;
+		}
+		double diff = f(x) / df(x);
+
+		while(Math.Abs(diff) > tol)
+		{
+			if(df(x) == 0)
+			{
+				Console.WriteLine("Error, df/dx == 0 where x = " + x);
+				return -1;
+			}
+			x = x - diff;
+			diff = f(x) / df(x);
+		}
+		Console.WriteLine("x = " + x);
+		return x;
+	}
+}
