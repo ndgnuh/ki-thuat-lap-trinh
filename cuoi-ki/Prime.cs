7e082195315af3e9a5f60cbe4d7a73af60c68ffa
ndgnuh <ndgnuh99@gmail.com>
2019-05-22 20:29:30 +0700

    3 version of prime

--- cuoi-ki/Prime.cs
+++ cuoi-ki/Prime.cs
@@ -10,11 +10,39 @@ public class PrimalityTest
 		return (n > 1) && true;
 	}
 
+	public static bool IsPrimeV2(int n)
+	{
+		for(int i = 2; i < n; i++)
+			if(n % i == 0)
+				return false;
+		return (n > 1) && true;
+	}
+
+	public static bool IsPrimeV3(int n)
+	{
+		for(int i = 2; i < n; i++)
+		{
+			for(int j = 1; j < n/i; j++)
+			{
+				if(n % (i * j) == 0) return false;
+			}
+		}
+		return (n > 1) && true;
+	}
+
 	public static void Main()
 	{
-		int n = 121;
+		if(IsPrime(13))
+			Console.WriteLine("is a prime");
+		else
+			Console.WriteLine("not a prime");
 
-		if(IsPrime(n))
+		if(IsPrimeV2(121))
+			Console.WriteLine("is a prime");
+		else
+			Console.WriteLine("not a prime");
+
+		if(IsPrimeV3(1))
 			Console.WriteLine("is a prime");
 		else
 			Console.WriteLine("not a prime");
